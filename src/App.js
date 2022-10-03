9cd259e1cfc5f69340500ef81ebcae7c50e21a0f
cmrodulfo <cmarin_rodulfo@yahoo.com>
2022-10-02 22:13:18 -0300

    iniciando NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from './compenets/Header/Header';
+import { Container, ThemeProvider } from 'react-bootstrap';
 
 function App() {
+
+  const breakpoints = ['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <Header />
+      <Container fluid classname='px-0'>
+        <ThemeProvider
+          breakpoints= { breakpoints }
+          minBreakpoint='xxs'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        </ThemeProvider>
+
+      </Container>
+    </>
   );
 }
 
